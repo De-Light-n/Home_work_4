276689221677513a57f7b3aaeceb7cb6dd59bf06
DeLight <nazarchukyayko@gmail.com>
2024-07-17 15:29:52 +0300

    homework 4 (1-4)

---
+++ hw_4_1.py
@@ -0,0 +1,33 @@
+def total_salary(path):
+    try:
+        with open(path, encoding="utf-8") as fh:
+            workers = fh.readlines()
+            
+        workers = [worker for worker in workers if worker.strip()] # cleaning from empty lines
+        error_count = 0
+        workers_count = 0
+        
+        if workers:
+            all_salary = 0 
+            for index, worker in enumerate(workers):
+                try:
+                    _, salary = worker.split(",")
+                    salary = float(salary)
+                    all_salary += salary  
+                    workers_count+=1
+                except Exception as e:
+                    error_count += 1
+                    print(f"In line {index + 1}:'{worker.strip()}' {e}")
+        else:
+            print("Could not find any workers")
+
+    except Exception as e:
+        print(e)
+        return (0,0)
+    
+    if workers_count:    
+        return (all_salary, all_salary/workers_count)
+    else:
+        return (0,0)
+
+print(total_salary("for4_1.txt"))
